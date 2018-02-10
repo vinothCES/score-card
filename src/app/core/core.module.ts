a761d70860ae49be127efccad660452d1a0d14ad
Vinoth J <vinoth.jayaprakash@cesltd.com>
2018-02-10 16:24:08 +0530

    Created login component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
     CommonModule,
     NgbModule.forRoot()
   ],
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, LoginComponent],
   exports: [
     NavbarComponent
   ]
