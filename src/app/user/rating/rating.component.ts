c5201bb6293e1df5d15ea2fb91c9687a083efe81
Vinoth J <vinoth.jayaprakash@cesltd.com>
2018-02-12 01:58:32 +0530

    Added actual template for Rating component Added logic to handle modal open

--- src/app/user/rating/rating.component.ts
+++ src/app/user/rating/rating.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'ces-rating',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RatingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+  }
+
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+    }, (reason) => {
+    });
   }
 
 }
